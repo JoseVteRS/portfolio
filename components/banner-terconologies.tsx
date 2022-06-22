cf79bd90240da33b8442e40020fac5175baee544
Jose Ripoll <jose.ripoll@acrismatic.es>
2022-06-22 13:52:17 +0200

    change banner-tecnologies add little mark

--- components/banner-terconologies.tsx
+++ components/banner-terconologies.tsx
@@ -13,12 +13,70 @@ type Props = {};
 const BannerTecnologies = (props: Props) => {
   return (
     <div className="flex flex-wrap gap-5 items-center justify-around container w-2/3 mx-auto">
-      <BadgeWithIcon icon={JavascriptIcon} color="fill-yellow-400" size="h-12 w-12" />
-      <BadgeWithIcon icon={TypescriptIcon} color="fill-blue-600" size="h-12 w-12" />
-      <BadgeWithIcon icon={ReactIcon} color="fill-cyan-400" size="h-12 w-12" />
-      <BadgeWithIcon icon={NextJSIcon} color="fill-white" size="h-12 w-12" />
-      <BadgeWithIcon icon={NodeIcon} color="fill-yellow-400" size="h-12 w-12" />
-      <BadgeWithIcon icon={TailwindcssIcon} color="fill-white" size="h-12 w-12" />
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-green-600 rounded-full"></div>
+        <BadgeWithIcon
+          icon={JavascriptIcon}
+          color="fill-yellow-400"
+          size="h-12 w-12"
+        />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          4 years
+        </span>
+      </div>
+
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-yellow-600 rounded-full"></div>
+        <BadgeWithIcon
+          icon={TypescriptIcon}
+          color="fill-blue-600"
+          size="h-12 w-12"
+        />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          2 years
+        </span>
+      </div>
+
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-yellow-400 rounded-full"></div>
+        <BadgeWithIcon
+          icon={ReactIcon}
+          color="fill-cyan-400"
+          size="h-12 w-12"
+        />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          2 years
+        </span>
+      </div>
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-yellow-400 rounded-full"></div>
+        <BadgeWithIcon icon={NextJSIcon} color="fill-white" size="h-12 w-12" />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          2 years
+        </span>
+      </div>
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-orange-700 rounded-full"></div>
+        <BadgeWithIcon
+          icon={NodeIcon}
+          color="fill-yellow-400"
+          size="h-12 w-12"
+        />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          3 years
+        </span>
+      </div>
+      <div className="text-center bg-slate-900 p-4 rounded-xl shadow-xl relative">
+        <div className="absolute top-2 left-2 h-2 w-2 bg-green-600 rounded-full"></div>
+        <BadgeWithIcon
+          icon={TailwindcssIcon}
+          color="fill-white"
+          size="h-12 w-12"
+        />
+        <span className="text-slate-300 font-thin text-xs inline-block">
+          1 years
+        </span>
+      </div>
     </div>
   );
 };
